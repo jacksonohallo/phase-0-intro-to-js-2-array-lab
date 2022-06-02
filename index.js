935b675159c819b0e73e80306b30b97bdd36d400
jacksonohallo <jacksonohallo@gmail.com>
2022-06-02 07:59:45 +0300

    arrange arrays

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
+
 // Write your solution here!
+var cats = ["Milo", "Otis", "Garfield"]
+
+function  destructivelyAppendCat(name){
+    cats.push(name);
+    return cats;
+}
+
+function destructivelyPrependCat(name) {
+    
+    cats.unshift(name)
+    
+    return cats;
+}
+
+function destructivelyRemoveLastCat(name) {
+    
+    cats.pop();
+    return cats;
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift();
+    return cats;
+}
+
+function appendCat(name) {
+    
+    let newcatArray = [...cats, name];
+    return newcatArray;
+}
+
+function removeLastCat() {
+    let newArrays=cats.slice(0,cats.length-1)
+    
+    return newArrays;
+}
+
+function removeFirstCat() {
+    
+let firstCat= cats.slice(1)
+    return firstCat;
+
+}
+
+function prependCat(name) {
+    
+    let caats = [name, ...cats];
+    return caats;
+
+}
